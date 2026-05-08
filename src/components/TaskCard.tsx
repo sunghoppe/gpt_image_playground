3de62d22bc1a3e03a58637eb209434d7c058736a
sunghoppe <songxiaole@saipujianshen.com>
2026-05-08 16:26:06 +0800

    feat:图标修改

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -410,7 +410,7 @@ export default function TaskCard({
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
-                      d="M4 4v6h6M20 20v-6h-6M5.64 18.36A9 9 0 0018.36 5.64L20 4M4 20l1.64-1.64"
+                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
                     />
                   </svg>
                 </button>
